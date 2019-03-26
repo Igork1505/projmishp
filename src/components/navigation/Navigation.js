aa3d5f1aeb92d69eb0899cd751301322ec489111
Igor <igor.ku.96@mail.ru>
2019-03-26 21:18:58 +0200

    button in Registration #ChangeAvatar#

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,85 +5,30 @@ import  './Navigation.css'
 class Navigation extends React.Component{
     render() {
         return(
-
-                function ($) {
-
-                "use strict";
-
-                    const cfg = {
-                            scrollDuration: 800,
-                        },
-
-                        $WIN = $(window);
+            <div>
 
 
-                    const doc = document.documentElement;
-                doc.setAttribute('data-useragent', navigator.userAgent);
+                <input id="hamburger" className="hamburger" type="checkbox"/>
+                <label className="hamburger" htmlFor="hamburger">
+                    <i></i>
+                    <text>
+                        <close>close</close>
+                        <open>menu</open>
+                    </text>
+                </label>
+                <section className="drawer-list">
+                    <ul>
+                        <li><a href="#">Sign in</a></li>
+                        <li><a href="#">Calendar</a></li>
+                        <li><a href="#">Add event</a></li>
 
-                const clPreloader = function () {
-
-                $("html").addClass('cl-preload');
-
-                $WIN.on('load', function () {
-
-                $("#loader").fadeOut("slow", function () {
-
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-            });
-            };
-
-                const clMenuOnScrolldown = function () {
-
-                    const menuTrigger = $('.header-menu-toggle');
-
-                    $WIN.on('scroll', function () {
-
-                if ($WIN.scrollTop() > 150) {
-                menuTrigger.addClass('opaque');
-            } else {
-                menuTrigger.removeClass('opaque');
-            }
-
-            });
-            };
-
-                const clOffCanvas = function () {
-
-                    const menuTrigger = $('.header-menu-toggle'),
-                        nav = $('.header-nav'),
-                        closeButton = nav.find('.header-nav__close'),
-                        siteBody = $('body'),
-                        mainContents = $('section, footer');
-
-                    menuTrigger.on('click', function (e) {
-                e.preventDefault();
-                siteBody.toggleClass('menu-is-open');
-            });
-
-                closeButton.on('click', function (e) {
-                e.preventDefault();
-                menuTrigger.trigger('click');
-            });
-
-                siteBody.on('click', function (e) {
-                if (!$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span')) {
-                siteBody.removeClass('menu-is-open');
-            }
-            });
-
-            };
-
-                (function clInit() {
-
-                clPreloader();
-                clMenuOnScrolldown();
-                clOffCanvas();
+                    </ul>
+                </section>
+            </div>
 
 
-            })();
 
-            })
+        )
 
 
 
