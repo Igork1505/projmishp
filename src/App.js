c2776cbe11c54ce43ac5d08e7d00678ed1293e64
Igor <igor.ku.96@mail.ru>
2019-03-28 10:45:04 +0200

    +React Router and Navigation(css+HTML only)

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import Calendar from "./components/Registration/Calendar";
 import Navigation from "./components/navigation/Navigation";
 import {BrowserRouter, Route} from "react-router-dom";
 import {Switch} from "react-router";
+import NotificationList from "./components/NotificationList/NotificationList";
+
 class App extends Component {
     render() {
         return (
 
             <BrowserRouter>
-               <div>
-                   <Navigation/>
-                   <Switch>
-                       <Route path="/" component={Main} exact/>
-                       <Route path="/Registration" component={Registration}/>
-                       <Route path="/Calendar" component={Calendar}/>
-                       <Route path="/Homepage" component={Homepage} />
-
-                   </Switch>
-               </div>
+                <div>
+                    <Navigation/>
+                    <Switch>
+                        <Route path="/" component={Main} exact/>
+                        <Route path="/Registration" component={Registration}/>
+                        <Route path="/Calendar" component={Calendar}/>
+                        <Route path="/Homepage" component={Homepage}/>
+                        <Route path="/NotificationList" component={NotificationList}/>
+                    </Switch>
+                </div>
             </BrowserRouter>
 
 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -11,8 +11,8 @@ class Main extends React.Component{
             <div className="main container">
 
                 <img src={mishpaLogo} className="logotip" alt="logogo"/>
-                <h2>Shabbat in the family circle</h2>
-                <p>We're looking for family where you can spend your
+                <h2 className='h2'>Shabbat in the family circle</h2>
+                <p className='p'>We're looking for family where you can spend your
                     hugs or meet shabbat. Just sing in and start finding
                     a new shabbat family. More than 100 families
                     waiting for you!
